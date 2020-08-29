c3da72d96ddf882de7a33f7a9111aff6909c9090
Rick <rickvickery@gmail.com>
2020-08-29 16:56:18 -0400

    Fixed rules, fixed critical state, minor adjustments, day tracker added

---
+++ TeamVsTeam/DayTracker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamVsTeam
+{
+    enum Event
+    {
+        Low,
+        Middle,
+        High,
+        Gold,
+        EXP,
+        Boss,
+        Extra,
+        Less
+    }
+    public class DayTracker
+    {
+        public static int Day = 1;
+
+        public static void Goodnight()
+        {
+            Day += 1;
+        }
+
+        public static void EventLoader()
+        {
+            if(Day == 5)
+            {
+                GenerateEnemies();
+                GenerateEnemies();
+            }
+            else
+            {
+                GenerateEnemies();
+            }
+           
+
+            
+
+        }
+
+        public static void GenerateEnemies()
+        {
+            Random rand = new Random();
+            int enemyWave = rand.Next(3, 5);
+            for (int i = 0; i < enemyWave; i++)
+            {
+                Unit unit = new Unit();
+                unit.GenerateEnemyUnit();
+                EnemyTeam.team.Add(unit);
+            }
+        }
+    }
+}
